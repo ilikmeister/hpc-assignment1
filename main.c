afb1e5e9adb0eff6c721bccaae95a684feae50b5
ilikmeister <akhmet.i27@gmail.com>
2025-09-06 13:44:21 +0800

    fixed long options kh kw

--- main.c
+++ main.c
@@ -2,31 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+#include <getopt.h>
 #include "conv2d.h"
 
 void usage() {
     printf("Usage:\n");
-    printf("./conv_test -f infile -g kernel -o outfile\n");
-    printf("./conv_test -H h -W w -kH kh -kW kw [-f infile] [-g kernel] [-o outfile]\n");
+    printf("./conv_test [--f infile] [--g kernel] [--o outfile]\n");
+    printf("./conv_test [--height H] [--width W] [--kH kh] [--kW kw] [--f infile] [--g kernel] [--o outfile]\n");
 }
 
 int main(int argc, char *argv[]) {
     float **f = NULL, **g = NULL, **out = NULL;
-    int H = 0, W = 0, kH = 0, kW = 0, opt;
+    int H = 0, W = 0, kH = 0, kW = 0;
     char *f_in = NULL, *g_in = NULL, *out_file = NULL;
     int gen = 0;
     srand(time(NULL));
 
-    while ((opt = getopt(argc, argv, "f:g:o:H:W:kH:kW:")) != -1) {
+    static struct option long_options[] = {
+        {"f",      required_argument, 0, 'f'},
+        {"g",      required_argument, 0, 'g'},
+        {"o",      required_argument, 0, 'o'},
+        {"height", required_argument, 0, 'H'},
+        {"width",  required_argument, 0, 'W'},
+        {"kH",     required_argument, 0, 'a'}, // unique short code for kernel height
+        {"kW",     required_argument, 0, 'b'}, // unique short code for kernel width
+        {0, 0, 0, 0}
+    };
+
+    int option_index = 0;
+    int opt;
+    while ((opt = getopt_long(argc, argv, "f:g:o:H:W:a:b:", long_options, &option_index)) != -1) {
         switch(opt) {
             case 'f': f_in = optarg; break;
             case 'g': g_in = optarg; break;
             case 'o': out_file = optarg; break;
             case 'H': H = atoi(optarg); gen = 1; break;
             case 'W': W = atoi(optarg); gen = 1; break;
-            case 'kH': kH = atoi(optarg); gen = 1; break;
-            case 'kW': kW = atoi(optarg); gen = 1; break;
+            case 'a': kH = atoi(optarg); gen = 1; break;
+            case 'b': kW = atoi(optarg); gen = 1; break;
             default: usage(); return 1;
         }
     }
